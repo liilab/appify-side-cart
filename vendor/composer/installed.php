c89d966d7fd27bad6df02136f454a082626fa332
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2022-04-11 09:26:33 +0600

    Composer Init

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '533d5875d622e12cd21c334143cd5cc185ad3343',
+        'name' => 'sabbir2001/ajax-cart',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sabbir2001/ajax-cart' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '533d5875d622e12cd21c334143cd5cc185ad3343',
+            'dev_requirement' => false,
+        ),
+    ),
+);
