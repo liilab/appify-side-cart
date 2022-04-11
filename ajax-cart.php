533d5875d622e12cd21c334143cd5cc185ad3343
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2022-04-11 07:15:33 +0600

    initial commmit

---
+++ ajax-cart.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              liilab.com
+ * @since             1.0
+ * @package           lii-ajaxcart
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Ajax Cart
+ * Plugin URI:        liilab.com
+ * Description:       A plugin for woocommerce Ajax Site cart.
+ * Version:           1.0
+ * Author:            LIILab
+ * Author URI:        liilab.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       lii-ajaxcart
+ * Domain Path:       /languages
+ */
