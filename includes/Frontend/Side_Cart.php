d1394c31391579ae773a9e373a29583540e31b30
Raiyan Noory Rady <raiyannooryrady@gmail.com>
2022-06-01 17:02:12 +0600

    image,name and price display in frontend markup

--- includes/Frontend/Side_Cart.php
+++ includes/Frontend/Side_Cart.php
@@ -47,78 +47,78 @@ class Side_Cart
 
     public function update_item_quantity()
     {
-        // GLOBAL $woocommerce;
-        // $id = $_POST['product_id'];
-        // echo $id;
-        // $product = wc_get_product($id);
+         GLOBAL $woocommerce;
+        $id = $_POST['product_id'];
+        echo $id;
+        $product = wc_get_product($id);
 
-        // $carts = $woocommerce->cart;
-        // //$hi = $carts['cart_contents'];
-        // //print("<pre>".print_r( $carts,true)."</pre>");
-        // //echo $hi;
-        // //var_dump($carts[0]);
-        // foreach ($carts as $cart_item_keys => $cart_items) {
-        //     //$products  		= apply_filters( 'woocommerce_cart_item_product', $cart_items['data'], $cart_items, $cart_item_keys );
-        //     $name = $cart_items;
-        //     break;
-        // }
-        // foreach ($name as $nm) {
-        //     echo $nm["key"];
-        // }
-        // $id = $_POST['product_id'];
-        // $new_qty = $_POST['quantity'];
-        // global $woocommerce;
-        // $items = $woocommerce->cart->get_cart();
+        $carts = $woocommerce->cart;
+        $hi = $carts['cart_contents'];
+        print("<pre>".print_r( $carts,true)."</pre>");
+        echo $hi;
+        var_dump($carts[0]);
+        foreach ($carts as $cart_item_keys => $cart_items) {
+            $products  		= apply_filters( 'woocommerce_cart_item_product', $cart_items['data'], $cart_items, $cart_item_keys );
+            $name = $cart_items;
+            break;
+        }
+        foreach ($name as $nm) {
+            echo $nm["key"];
+        }
+        $id = $_POST['product_id'];
+        $new_qty = $_POST['quantity'];
+        global $woocommerce;
+        $items = $woocommerce->cart->get_cart();
 
-        // foreach ($items as $item => $values) {
-        //     $c_id = $values['product_id'];
-        //     if ($c_id == $id) :
-        //         $_product =  wc_get_product($values['data']->get_id());
+        foreach ($items as $item => $values) {
+            $c_id = $values['product_id'];
+            if ($c_id == $id) :
+                $_product =  wc_get_product($values['data']->get_id());
 
-        //         WC()->cart->set_quantity($item, $new_qty);
-        //         echo "<b>" . $_product->get_title() . '</b>  <br> Quantity: ' . $values['quantity'] . '<br>';
-        //         $price = get_post_meta($values['product_id'], '_price', true);
-        //         echo "  Price: " . $price . "<br>";
-        //     endif;
-        // }
-        // echo '';
+                WC()->cart->set_quantity($item, $new_qty);
+                echo "<b>" . $_product->get_title() . '</b>  <br> Quantity: ' . $values['quantity'] . '<br>';
+                $price = get_post_meta($values['product_id'], '_price', true);
+                echo "  Price: " . $price . "<br>";
+            endif;
+        }
+        echo '3333333333333333';
 
-        // \WC_AJAX::get_refreshed_fragments();
+        \WC_AJAX::get_refreshed_fragments();
 
-        // $hi = 123;
-        // $cart_key = sanitize_text_field($_POST['cart_key']);
-        // $new_qty  = (float) $_POST['qty'];
+        $hi = 123;
+        $cart_key = sanitize_text_field($_POST['cart_key']);
+        $new_qty  = (float) $_POST['qty'];
 
-        // if (!is_numeric($new_qty) || $new_qty < 0 || !$cart_key) {
-        //     //$this->set_notice( __( 'Something went wrong', 'side-cart-woocommerce' ) );
-        // }
+        if (!is_numeric($new_qty) || $new_qty < 0 || !$cart_key) {
+            //$this->set_notice( __( 'Something went wrong', 'side-cart-woocommerce' ) );
+        }
 
-        // $validated = apply_filters('lii_ajaxcart_update_quantity', true, $cart_key, $new_qty);
+        $validated = apply_filters('lii_ajaxcart_update_quantity', true, $cart_key, $new_qty);
 
-        // if ($validated && !empty(WC()->cart->get_cart_item($cart_key))) {
+        if ($validated && !empty(WC()->cart->get_cart_item($cart_key))) {
 
-        //     $updated = $new_qty == 0 ? WC()->cart->remove_cart_item($cart_key) : WC()->cart->set_quantity($cart_key, $new_qty);
+            $updated = $new_qty == 0 ? WC()->cart->remove_cart_item($cart_key) : WC()->cart->set_quantity($cart_key, $new_qty);
 
-        //     if ($updated) {
+            if ($updated) {
 
-        //         if ($new_qty == 0) {
+                if ($new_qty == 0) {
 
-        //             $notice = __('Item removed', 'side-cart-woocommerce');
+                    $notice = __('Item removed', 'side-cart-woocommerce');
 
-        //             $notice .= '<span class="xoo-wsc-undo-item" data-key="' . $cart_key . '">' . __('Undo?', 'side-cart-woocommerce') . '</span>';
-        //         } else {
-        //             $notice = __('Item updated', 'side-cart-woocommerce');
-        //         }
+                    $notice .= '<span class="xoo-wsc-undo-item" data-key="' . $cart_key . '">' . __('Undo?', 'side-cart-woocommerce') . '</span>';
+                } else {
+                    $notice = __('Item updated', 'side-cart-woocommerce');
+                }
 
-        //         //$this->set_notice( $notice, 'success' );
+                //$this->set_notice( $notice, 'success' );
 
-        //     }
-        // }
+            }
+        }
 
-        // $this->get_refreshed_fragments();
+        //$this->get_refreshed_fragments();
 
 
-        // die();
+        die();
     }
 }
 
