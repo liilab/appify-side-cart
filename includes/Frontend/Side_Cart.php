b90fb42b44e4ef32581b05ba726a6a25f319147c
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2022-06-06 15:36:18 +0600

    ajax cart all functionality add

--- includes/Frontend/Side_Cart.php
+++ includes/Frontend/Side_Cart.php
@@ -41,9 +41,22 @@ class Side_Cart
 
     public function hooks()
     {
-        add_action('wc_ajax_lii_ajaxcart_add_to_cart', [$this, 'add_to_cart']);
+        add_action('wc_ajax_lii_ajaxcart_add_to_cart', [$this, 'update_item_quantity']);
         add_filter('woocommerce_add_to_cart_fragments', [$this, 'set_ajax_fragments']);
+        // add_action('wp_print_scripts', function(){
+        //     wp_dequeue_script( 'wc-cart-fragments' );
+        //     return true;
+        // });
+        // add_action('wp_enqueue_scripts', function () {
+        //     global $woocommerce;
+
+        //     $suffix = defined('SCRIPT_DEBUG') ?: '.min';
+        //     wp_deregister_script('jquery-cookie');
+        //     wp_register_script('jquery-cookie', $woocommerce->plugin_url() . '/assets/js/jquery-cookie/jquery_cookie' . $suffix . '.js', array('jquery'), '1.3.1', true);
+        // });
     }
+
+
 
     /**
      * Create Fragment
@@ -52,7 +65,7 @@ class Side_Cart
 
     public function set_ajax_fragments($fragments)
     {
-        $fragments['span.lii-cart-count']     = '<span class="lii-cart-count">' . count( WC()->cart->get_cart()) . '</span>';
+        $fragments['span.lii-cart-count']     = '<span class="lii-cart-count">' . count(WC()->cart->get_cart()) . '</span>';
         $fragments['span.lii-subtotal-price'] = '<span class="lii-subtotal-price">' . WC()->cart->get_cart_subtotal() . '</span>';
         $fragments['span.lii-shipping-price'] = '<span class="lii-shipping-price">' . WC()->cart->get_shipping_total() . '</span>';
         $fragments['span.lii-total-price']    = '<span class="lii-total-price">' . WC()->cart->get_total() . '</span>';
@@ -68,8 +81,36 @@ class Side_Cart
      * Add to Cart Action
      */
 
-    public function add_to_cart()
+    public function update_item_quantity()
     {
+        $cart_key     = sanitize_text_field($_POST['cart_key']);
+        $new_qty     = (float) $_POST['qty'];
+
+        if (!is_numeric($new_qty) || $new_qty < 0 || !$cart_key) {
+            //$this->set_notice( __( 'Something went wrong', 'side-cart-woocommerce' ) );
+        }
+
+        $validated = apply_filters('xoo_wsc_update_quantity', true, $cart_key, $new_qty);
+
+        if ($validated && !empty(WC()->cart->get_cart_item($cart_key))) {
+
+            $updated = $new_qty == 0 ? WC()->cart->remove_cart_item($cart_key) : WC()->cart->set_quantity($cart_key, $new_qty);
+
+            if ($updated) {
+
+                if ($new_qty == 0) {
+
+                    $notice = __('Item removed', 'side-cart-woocommerce');
+
+                    $notice .= '<span class="xoo-wsc-undo-item" data-key="' . $cart_key . '">' . __('Undo?', 'side-cart-woocommerce') . '</span>';
+                } else {
+                    $notice = __('Item updated', 'side-cart-woocommerce');
+                }
+
+                //$this->set_notice( $notice, 'success' );
+
+            }
+        }
 
         \WC_AJAX::get_refreshed_fragments();
         die();
