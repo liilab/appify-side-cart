b1b9e29e3dd56784efebe0bd1f07a8abd9e337b0
Raiyan Noory Rady <raiyannooryrady@gmail.com>
2022-06-09 15:22:35 +0600

    appropriate commenting

--- templates/shipping-address.php
+++ templates/shipping-address.php
@@ -9,13 +9,14 @@
         <div class="lii-ship-destination">
             <span>Shipping to:</span><span><?php echo WC()->customer->get_shipping_country(); ?></span>
         </div>
-        <form action="" class="lii-ship-form">
+        <form action="" method="post" class="lii-ship-form">
             <!-- <select class="form-select form-select-sm lii-form-select" aria-label=".form-select-sm example">
                         <option selected>Bangladesh</option>
                         <option value="1">India</option>
                         <option value="2">Pakistan</option>
                         <option value="3">England</option>
                     </select> -->
+            <!-- Billing Country -->
             <?php
             woocommerce_form_field(
                 'billing_country',
@@ -24,6 +25,7 @@
                     'input_class' => array('form-select form-select-sm lii-form-select example')
                 )
             );
+            // Billing State
             woocommerce_form_field(
                 'billing_state',
                 array(
@@ -34,9 +36,11 @@
             );
 
             ?>
-
+            <!-- Shipping City -->
             <input type="text" class="lii-input-text" value="<?php echo $woocommerce->customer->get_shipping_city(); ?>" placeholder="City" name="calc_shipping_city" id="calc_shipping_city">
+            <!-- Shipping Post Code -->
             <input type="text" class="lii-input-text" value="<?php echo $woocommerce->customer->get_shipping_postcode(); ?>" placeholder="Postcode / ZIP" name="calc_shipping_postcode" id="calc_shipping_postcode">
+            <!-- Shipping Button -->
             <div class="lii-btn-area"><button type="submit" name="calc_shipping" value="1" class="lii-button">Update</button></div>
         </form>
     </div>
