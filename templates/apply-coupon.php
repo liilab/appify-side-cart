40bfefc8330e600862d20502bc7bd4fcc0b2bd14
Raiyan Noory Rady <raiyannooryrady@gmail.com>
2022-06-13 00:24:36 +0600

    invalid coupon part

--- templates/apply-coupon.php
+++ templates/apply-coupon.php
@@ -23,8 +23,42 @@
                 </div>
 
             </div>
+            
             <div class="lii-unavail-coupons lii-coupons">
-                <span class="lii-coupon-label">Unvailable Coupons</span>
+                <span class="lii-coupon-label">Unavailable Coupons</span>
+
+
+                <div class="lii-coupon-row">
+                    <span class="lii-cr-code">off10</span>
+                    <span class="lii-cr-off">Get 10% off</span>
+                    <span class="lii-cr-desc">Use code OFF10 &amp; get 10% discount on orders above $50</span>
+                </div>
+
+
+
+                <div class="lii-coupon-row">
+                    <span class="lii-cr-code">off20</span>
+                    <span class="lii-cr-off">Get 20% off</span>
+                    <span class="lii-cr-desc">Use code OFF20 &amp; get 20% discount on orders above $100</span>
+                </div>
+
+
+
+                <div class="lii-coupon-row">
+                    <span class="lii-cr-code">off25</span>
+                    <span class="lii-cr-off">Get 25% off</span>
+                    <span class="lii-cr-desc">Use code OFF25 &amp; get 25% discount on orders above $500</span>
+                </div>
+
+
+
+                <div class="lii-coupon-row">
+                    <span class="lii-cr-code">off30</span>
+                    <span class="lii-cr-off">Get 30% off</span>
+                    <span class="lii-cr-desc">Use code OFF30 &amp; get 30% discount on orders above $600</span>
+                </div>
+
+
             </div>
         </div>
     </div>
