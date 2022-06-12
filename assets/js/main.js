c221683804223a8b265fb6577d4b280b8d9cc206
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2022-06-12 19:40:02 +0600

    add to cart no load feature add & code arrange

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,110 +78,75 @@ var updateFragments = function (response) {
 
     if (response.fragments) {
 
-        //Set fragments
-
         $.each(response.fragments, function (key, value) {
             $(key).replaceWith(value);
         });
-
-        if (typeof wc_cart_fragments_params !== 'undefined' && ('sessionStorage' in window && window.sessionStorage !== null)) {
-
-            sessionStorage.setItem(wc_cart_fragments_params.fragment_name, JSON.stringify(response.fragments));
-            localStorage.setItem(wc_cart_fragments_params.cart_hash_key, response.cart_hash);
-            sessionStorage.setItem(wc_cart_fragments_params.cart_hash_key, response.cart_hash);
-
-            if (response.cart_hash) {
-                sessionStorage.setItem('wc_cart_created', (new Date()).getTime());
-            }
-
-        }
-
-        $(document.body).trigger('wc_fragments_refreshed');
     }
-
-    $(document.body).trigger('wc_fragment_refresh');
 }
 
-//=== On change event on product item Input Area ===//
+//===Single product page load off===//
 
-$(document).on('change', 'input.lii-input-text', function (e) {
+$(document).on('click', '.single_add_to_cart_button', function (e) {
     e.preventDefault();
-
-    product_key = $(this).attr('data-key');
-    quantity = $(this).val();
-
-    console.log(product_key);
-    var data = {
-        action: 'update_item_quantity',
-        product_key: product_key,
-        qty: quantity,
-    };
-
-    $.ajax({
-        type: 'POST',
-        url: get_wcurl('lii_ajaxcart_add_to_cart'),
-        data: data,
-        success: function (response) {
-            updateFragments(response);
-        },
-    });
+    $thisbutton = $(this),
+        $form = $thisbutton.closest('form.cart'),
+        id = $thisbutton.val(),
+        quantity = $form.find('input[name=quantity]').val() || 1,
+        productID = $form.find('input[name=product_id]').val() || id,
+        variation_id = $form.find('input[name=variation_id]').val() || 0;
+    add_to_cart(productID, quantity, variation_id);
 });
 
-//=== Product Decrement By Ajax ===//
+//===Shop page load off===//
 
-$(document).on('click', '.lii-minus', function (e) {
+$(document).on('submit', 'form.cart', function (e) {
+
+    var $form = $(e.currentTarget);
+
+    if ($form.closest('.product').hasClass('product-type-external')) return;
+
     e.preventDefault();
 
-    var ele = $(this).next('.lii-qty'),
-        quantity = parseInt(ele.val());
+    $thisbutton = $form.find('button[type="submit"]');
+    var is_url = $form.attr('action').match(/add-to-cart=([0-9]+)/),
+        productID = is_url ? is_url[1] : false;
 
-    var el = $(this).next('input'),
-        product_key = el.attr('data-key');
+    quantity = $form.find('input[name=quantity]').val() || 1,
+        variation_id = $form.find('input[name=variation_id]').val() || 0;
 
-    var data = {
-        action: 'update_item_quantity',
-        product_key: product_key,
-        qty: quantity,
-    };
-
-    $.ajax({
-        type: 'POST',
-        url: get_wcurl('lii_ajaxcart_add_to_cart'),
-        data: data,
-        success: function (response) {
-            updateFragments(response);
-        },
-    });
-
+    add_to_cart(productID, quantity, variation_id);
 });
 
-//=== Product Increament By Ajax ===//
+//=== add to cart function ===//
 
-$(document).on('click', '.lii-plus', function (e) {
-    e.preventDefault();
-
-    var ele = $(this).prev('.lii-qty'),
-        quantity = parseInt(ele.val());
-
-    var el = $(this).prev('input'),
-        product_key = el.attr('data-key');
-
+var add_to_cart = function (productID, product_qty, variation_id) {
     var data = {
         action: 'update_item_quantity',
-        product_key: product_key,
-        qty: quantity,
+        product_id: productID,
+        product_sku: '',
+        quantity: product_qty,
+        variation_id: variation_id,
     };
-
     $.ajax({
-        type: 'POST',
+        type: 'post',
         url: get_wcurl('lii_ajaxcart_add_to_cart'),
         data: data,
+        beforeSend: function (response) {
+            $thisbutton.removeClass('added').addClass('loading');
+        },
+        complete: function (response) {
+            $thisbutton.addClass('added').removeClass('loading');
+        },
         success: function (response) {
-            updateFragments(response);
+            if (response.error & response.product_url) {
+                window.location = response.product_url;
+                return;
+            } else {
+                $(document.body).trigger('added_to_cart', [response.fragments, response.cart_hash, $thisbutton]);
+            }
         },
     });
-
-});
+}
 
 //=== Product Delete By Ajax ===//
 
@@ -191,6 +156,24 @@ $(document).on('click', '.lii-trash', function (e) {
     quantity = 0;
     product_key = $(this).attr('data-key');
 
+    update_item_quantity(product_key, quantity);
+});
+
+//=== On change event on product item Input Area ===//
+
+$(document).on('change', 'input.lii-input-text', function (e) {
+    e.preventDefault();
+
+    product_key = $(this).attr('data-key');
+    quantity = $(this).val();
+
+    update_item_quantity(product_key, quantity);
+
+});
+
+//===update quantity function===//
+
+var update_item_quantity = function (product_key, quantity) {
     var data = {
         action: 'update_item_quantity',
         product_key: product_key,
@@ -199,14 +182,13 @@ $(document).on('click', '.lii-trash', function (e) {
 
     $.ajax({
         type: 'POST',
-        url: get_wcurl('lii_ajaxcart_add_to_cart'),
+        url: get_wcurl('lii_ajaxcart_update_item_quantity'),
         data: data,
         success: function (response) {
             updateFragments(response);
         },
     });
-
-});
+}
 
 
 
