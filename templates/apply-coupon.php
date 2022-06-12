fd37ca8e4fb9ce7bd5f79ace385569c96433116c
Raiyan Noory Rady <raiyannooryrady@gmail.com>
2022-06-13 00:21:24 +0600

    valid coupon part

--- templates/apply-coupon.php
+++ templates/apply-coupon.php
@@ -10,5 +10,22 @@
             <input type="text" class="lii-input-text" name="lii-coupon-input" placeholder="Enter Promo Code">
             <button class="lii-button" type="submit">Submit</button>
         </form>
+        <div class="lii-coupon-items">
+
+            <div class="lii-avail-coupons lii-coupons">
+
+                <span class="lii-coupon-label">Available Coupons</span>
+                <div class="lii-coupon-row">
+                    <span class="lii-cr-code">off5</span>
+                    <span class="lii-cr-off">Get 5% off</span>
+                    <span class="lii-cr-desc">Use code OFF5 &amp; get 5% discount on orders above $15</span>
+                    <button class="lii-button" value="off5">Apply Coupon</button>
+                </div>
+
+            </div>
+            <div class="lii-unavail-coupons lii-coupons">
+                <span class="lii-coupon-label">Unvailable Coupons</span>
+            </div>
+        </div>
     </div>
 </div>
