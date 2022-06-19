50c7e4ef712e0780a15801a2960a2aaf11a66a0c
Raiyan Noory Rady <raiyannooryrady@gmail.com>
2022-06-19 23:28:49 +0600

    lii-set coupon

--- templates/apply-coupon.php
+++ templates/apply-coupon.php
@@ -8,21 +8,23 @@
     <div class="lii-coupon-body">
         <!-- <form method="post" class="lii-apply-coupon">
             <input type="text" class="lii-input-text" name="lii-coupon-input" placeholder="Enter Promo Code">
-            <button class="lii-button" type="submit" name="apply_coupon" value="<?php //esc_attr_e('Apply coupon', 'woocommerce'); ?>">Submit</button>
+            <button class="lii-button" type="submit" name="apply_coupon" value="<?php //esc_attr_e('Apply coupon', 'woocommerce'); 
+                                                                                ?>">Submit</button>
         </form> -->
+        <div class="lii-set-coupon">
+            <?php
+            echo do_shortcode("[coupon_field]");
+            $coupons = WC()->cart->get_coupons();
+            if (!empty($coupons)) : ?>
 
-        <?php 
-        echo do_shortcode("[coupon_field]");
-        $coupons = WC()->cart->get_coupons();
-        if (!empty($coupons)) : ?>
+                <ul class="lii-applied-coupons mb-4">
+                    <?php foreach ($coupons as $code => $coupon) : ?>
+                        <li class="lii-remove-coupon" data-coupon="<?php echo $code; ?>"><?php echo $code; ?></li>
+                    <?php endforeach; ?>
+                </ul>
 
-            <ul class="lii-applied-coupons mb-4">
-                <?php foreach ($coupons as $code => $coupon) : ?>
-                    <li class="lii-remove-coupon" data-coupon="<?php echo $code; ?>"><?php echo $code; ?></li>
-                <?php endforeach; ?>
-            </ul>
-
-        <?php endif; ?>
+            <?php endif; ?>
+        </div>
 
         <div class="lii-coupon-items">
 
