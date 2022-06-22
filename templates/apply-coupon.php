4f431ed60a8e26e9929ba82bc9e565b271fd3a58
Raiyan Noory Rady <raiyannooryrady@gmail.com>
2022-06-22 18:34:22 +0600

    restructure templates

--- templates/apply-coupon.php
+++
@@ -1,7 +0,0 @@
-<?php global $woocommerce;
-?>
-<div id="lii-coupon-area" class="lii-coupon-area">
-    <?php require_once LII_AJAXCART_DIR_PATH . 'templates/coupon-header.php'; ?>
-    <?php require_once LII_AJAXCART_DIR_PATH . 'templates/coupon-body.php'; ?>
-    
-</div>
---
+++ templates/coupon/apply-coupon.php
@@ -0,0 +1,7 @@
+<?php global $woocommerce;
+?>
+<div id="lii-coupon-area" class="lii-coupon-area">
+    <?php require_once LII_AJAXCART_DIR_PATH . 'templates/coupon/coupon-header.php'; ?>
+    <?php require_once LII_AJAXCART_DIR_PATH . 'templates/coupon/coupon-body.php'; ?>
+    
+</div>
