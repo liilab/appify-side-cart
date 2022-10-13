4bc12bff586cf4c35d1e2a4a65c66f9eab961da3
SABBIR AHMED TALUKDAR <satsabbir11@gmail.com>
2022-10-13 19:46:45 +0600

    Change option redux to wordpress option.

--- templates/main-contents.php
+++ templates/main-contents.php
@@ -1,59 +1,63 @@
 <?php
-$var1        = 1;
-$var2        = 1;
-$var3        = 1;
 $inlineStyle = '';
-if (class_exists('Redux')) :
-    $var1        = \Redux::get_option('redux_demo', 'shoppage-load');
-    $var2        = \Redux::get_option('redux_demo', 'singlepage-load');
-    $var3        = \Redux::get_option('redux_demo', 'product-quantity-box');
-    $hidden      = ' ';
-    $inlineStyle = ' ';
-    if (!$var3) {
-        $hidden      = 'd-none';
-        $inlineStyle = 'style="margin-left:0; padding: 0;"';
-    }
-endif;
+
+$var1 = get_option( 'Lii-ajax-add-to-cart-option' ); //shop-page-load
+$var2 = get_option( 'Lii-ajax-single-product-page-add-to-cart-option' );  //single-product-page-load
+$var3 = get_option( 'Lii-showing-product-quantity-box-option' );  //product-quantity-box
+
+$var4 = get_option( 'Lii-cart-order-option' ); //cart-order
+
+$hidden      = ' ';
+$inlineStyle = ' ';
+
+
+if ( $var3 ) {
+	$hidden      = 'd-none';
+	$inlineStyle = 'style="margin-left:0; padding: 0;"';
+}
 ?>
-<div class="lii-main-contents <?php if ($var1) {
-                                    echo 'shoppage-load';
-                                }
-                                if ($var2) {
-                                    echo ' singlepage-load';
-                                } ?>">
+<div class="lii-main-contents <?php if ( $var1 ) {
+	echo 'shoppage-load';
+}
+if ( $var2 ) {
+	echo ' singlepage-load';
+} ?>">
     <!--Cart Products-->
-    <?php
-    global $woocommerce;
-    $items = $woocommerce->cart;
-    $ii = 0;
+	<?php
+	$index = 0;
 
-    foreach (WC()->cart->get_cart() as $cart_item_key => $cart_item) :
-        $product           = $cart_item['data'];
-        $thumbnail         = $product->get_image();
-        $product_permalink = $product->get_permalink($cart_item);
-        $sub_total         = WC()->cart->get_product_subtotal($cart_item['data'], $cart_item['quantity']);
+	$items = WC()->cart->get_cart();
+	if($var4) {$items = array_reverse( $items, true );}
 
-        require LII_AJAXCART_DIR_PATH . 'templates/products.php';
-        $ii++;
-    endforeach;
-    if ($ii == 0) {
-        $shop_url = get_permalink( wc_get_page_id( 'shop' ));
-    ?>
+	foreach ( $items as $cart_item_key => $cart_item ) :
+		$product           = $cart_item['data'];
+		$thumbnail         = $product->get_image();
+		$product_permalink = $product->get_permalink( $cart_item );
+		$sub_total         = WC()->cart->get_product_subtotal( $cart_item['data'], $cart_item['quantity'] );
+
+		require LII_AJAXCART_DIR_PATH . 'templates/products.php';
+
+		$index ++;
+	endforeach;
+
+	if ( $index == 0 ) {
+		$shop_url = get_permalink( wc_get_page_id( 'shop' ) );
+		?>
         <div>
             <img src="<?php echo LII_AJAXCART_ASSETS . '/img/empty_cart1.png' ?>" alt="" style="
             width: 250px;
             height: auto;
             margin-left: 65px;
     ">
-    <div class="text-center">
-            <h3 class="fw-bold"><?php _e('Your cart is empty!','lii-ajax-cart'); ?></h3>
-            <button onclick="window.location.href='<?php echo $shop_url; ?>';" class="mt-2" style="    margin-left: 5px;
+            <div class="text-center">
+                <h3 class="fw-bold"><?php _e( 'Your cart is empty!', 'lii-ajax-cart' ); ?></h3>
+                <button onclick="window.location.href='<?php echo $shop_url; ?>';" class="mt-2" style="    margin-left: 5px;
     background-color: black;
     color: white;
     padding: 10px 42px;
     border: none;
-    font-weight: 600;"><?php _e('Go to Shop','lii-ajax-cart'); ?></button>
+    font-weight: 600;"><?php _e( 'Go to Shop', 'lii-ajax-cart' ); ?></button>
+            </div>
         </div>
-        </div>
-    <?php } ?>
+	<?php } ?>
 </div>
